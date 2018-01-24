6ba8e130c6d4895fabb748d9fca8c503608b2ab3
hchristensen14 <35748693+hchristensen14@users.noreply.github.com>
2018-01-24 15:51:15 -0600

    Add files via upload

---
+++ githubHello.c
@@ -0,0 +1,16 @@
+/*
+ *
+ *
+ *
+ */
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+int main(int argc, char **argv) {
+
+	printf("Hello it's Hunter Christensen\n");
+	
+	return(0);
+}
